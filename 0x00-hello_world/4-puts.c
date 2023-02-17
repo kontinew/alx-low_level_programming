84f546b407fcde1b73cb40122a57da778f87ce26
Kamenika <kamenikagovender21@gmail.com>
2023-02-17 00:03:47 -0800

    Update 4-puts.c (6)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n";
+	char str = "Programming is like building a multilingual puzzle\n";
+	puts(str);
 	return 0;
 }
