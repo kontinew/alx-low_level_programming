febd38ff48522103445a984baebcc811a4cfa344
Kamenika <kamenikagovender21@gmail.com>
2023-02-22 01:19:54 -0800

    Task 4 update3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/*Function declaration*/
 int main(void)
 {
+	/*char declaration*/
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	
+	/*Use of puts function*/
 	puts(str);
+
 	return (0);
 }
