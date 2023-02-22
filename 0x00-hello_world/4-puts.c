75b94f7aed281d698057e6fc519e04bfbd0ca9fa
Kamenika <kamenikagovender21@gmail.com>
2023-02-22 01:42:24 -0800

    Task 4 update 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-/*Function declaration*/
+/**
+ * 4-puts.c - The puts function
+ * Description: Output a string variable using the puts function instead of the printf function
+ * Section header: 
+ * Return: 0
+ * */
 int main(void)
 {
-	/*char declaration*/
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	
-	/*Use of puts function*/
-	puts(str);
 
+	puts(str);
 	return (0);
+
 }
