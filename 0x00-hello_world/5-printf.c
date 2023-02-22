844a62676a9d6a64e362e53dab69b72dd1eeb721
Kamenika <kamenikagovender21@gmail.com>
2023-02-22 15:46:49 -0800

    Creation of 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints string using the printf function
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
