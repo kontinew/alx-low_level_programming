6db345030dd731ab584139998bfab43dae6b9ea3
Kamenika <kamenikagovender21@gmail.com>
2023-02-22 16:38:28 -0800

    Creation of 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print size of various types
+ * Return: 0
+ **/
+int main(void)
+{
+	char charSize;
+	int intSize;
+	long int longintSize;
+	long long int longlongintSize;
+
+	printf("Size of a char: " + charSize);
+	printf("Size of an int: " + intSize);
+	printf("Size of a long int: " + longintSize);
+	printf("Size of a long long int: " + longlongintSize);
+	printf("Size of a float: " + floatSize);
+
+	return (0);
+}
