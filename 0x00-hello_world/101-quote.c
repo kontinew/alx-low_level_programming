27a7007064daa2361bfe89e44ee390e436e2a99e
Kamenika <kamenikagovender21@gmail.com>
2023-02-25 23:57:08 -0800

    Task 8 Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <syscall.h>
 /**
  * main - Print a quote without using printf nor put
  * Returns: 1
  **/
 int main(void)
 {
-	const char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	syscall(SYS_write, 1, quote, sizeof(quote));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
