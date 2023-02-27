225e0301844ccbcf7018e46857a1435624b18915
Kamenika <kamenikagovender21@gmail.com>
2023-02-27 00:31:13 -0800

    Task 2 Creation

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet using putchar
+ * Return: 0
+ **/
+int main(void)
+{
+	char ch = 'a';
+	
+	while(ch <= 'z')
+	{
+		printf("%c", ch);
+		ch++;
+	}
+	printf("\n");
+	return (0);
+}
