08e5bc64a6ecc6b79a6eee12e4ae4c01154b3b24
Kamenika <kamenikagovender21@gmail.com>
2023-02-26 21:09:04 -0800

    Task 8 Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  **/
 int main(void)
 {
-	write(1, "and that piece of art is", sizeof("and that piece of art is"));
-	write(1,  " useful\" - Dora Korpar", sizeof(" useful\" - Dora Korpar"));
-	write(1, ", 2015-10-19\n", sizeof(", 2015-10-19\n"));
+	char quote;
+
+	quote =	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quote, sizeof(quote));
 	return (1);
 }
