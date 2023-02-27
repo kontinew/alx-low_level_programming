63c80e625df7e55f9d51270c432b9a18ef28179d
Kamenika <kamenikagovender21@gmail.com>
2023-02-26 20:12:40 -0800

    Task 8 Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 /**
  * main - Print a quote without using printf nor put
- * Returns: 1
+ * Return: 1
  **/
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(1, "and that piece of art is", sizeof("and that piece of art is"));
+	write(1,  " useful\" - Dora Korpar", sizeof(" useful\" - Dora Korpar"));
+	write(1, ", 2015-10-19\n", sizeof(", 2015-10-19\n"));
 	return (1);
 }
