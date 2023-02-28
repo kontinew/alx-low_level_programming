9e822e7babea95b21f7c926e44c3606348988625
Kamenika <kamenikagovender21@gmail.com>
2023-02-27 19:43:53 -0800

    Task 2 update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,20 @@
+#include "main.h"
 /**
  * _puts - prints a string
  * @str: the string variable
  **/
 void _puts(char *str)
 {
-	char ch[] = &str;
-	puts(ch);
+	char ch[] = str;
+
+	int len = _strlen(str);
+
+	int count;
+
+	while (count >= len)
+	{
+		_putchar(ch[count];
+		count++;
+	}
+	_putchar('\n');
 }
