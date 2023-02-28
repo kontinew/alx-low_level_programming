59e0267dbe9445128847737dbc218c93715e88f7
Kamenika <kamenikagovender21@gmail.com>
2023-02-27 19:33:20 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to find the length of
+ *
+ * Return: the length of the string
+ **/
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(&s);
+	return (len);
+}
