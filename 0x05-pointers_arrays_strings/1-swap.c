d36bd28752a56a924a111dcfb4b90139a8b03552
Kamenika <kamenikagovender21@gmail.com>
2023-02-27 18:51:45 -0800

    Task 1 Creation

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swaps 2 integers around
+ * @&a - first integer to become b
+ * @&b - second integer to become a
+ **/
+void swap_int(int &a, int &b)
+{
+	int n = &a;
+
+	&a = &b;
+	&b = n;
+}
