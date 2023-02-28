29acc6873d3e344ce4bb67ef9dbc3a5895c32cfa
Kamenika <kamenikagovender21@gmail.com>
2023-02-27 17:52:09 -0800

    task 0 update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main - print _putchar
  * Return: 0
  **/
 int main(void)
 {
-	putchar('_' + 'p' + 'u' + 't' + 'c' + 'h' + 'a' + 'r' + '\n');
+	char str[] = "_putchar";
+
+	int n;
+
+	for (n = 0; n < sizeof(str); n++)
+	{
+		putchar(str[n]);
+	}
+	putchar('\n');
 	return (0);
 }
