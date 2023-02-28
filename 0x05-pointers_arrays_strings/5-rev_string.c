3495bf8964360ee0a071968ab3ccada32a3b3eac
Kamenika <kamenikagovender21@gmail.com>
2023-02-28 11:24:05 -0800

    Task 5 Creation

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses a string
+ * @s: a string to be reversed
+ **/
+void rev_string(char *s)
+{
+	int len, temp, n, i;
+
+	len = 0;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		len++;
+	}
+
+	for (i = 0; i < len/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
