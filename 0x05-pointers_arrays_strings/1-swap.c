85647834b59f642460ebf46822b30982f746dc75
Kamenika <kamenikagovender21@gmail.com>
2023-02-27 18:58:05 -0800

    Task 1 update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 /**
  * swap_int - swaps 2 integers around
- * @*a - first integer to become b
- * @*b - second integer to become a
+ * @a - first integer to become b
+ * @b - second integer to become a
  **/
 void swap_int(int *a, int *b)
 {
