b152b707f938ba63f3c2c5fde163ecb84e6f0527
Kamenika <kamenikagovender21@gmail.com>
2023-03-02 15:35:29 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * src: source string
+ * n: amount of bytes
+ **/
+char *_strncat(char *dest, char *src int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j <= n)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	
+	return (dest);
+}
