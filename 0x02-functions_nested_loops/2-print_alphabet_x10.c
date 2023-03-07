1059d1c75ee05ee1ed3e60cad03270e55998f4ad
Kamenika <kamenikagovender21@gmail.com>
2023-03-07 12:32:31 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		
+		_putchar('\n');
+	}
+}
